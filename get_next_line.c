e86855547cd9acf68a9d9ba09d1e251e74cd2ff8
Maksym Petrunok <mpetruno@student.unit.ua>
2018-08-14 18:41:02 +0300

    Initial commit.

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpetruno <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 18:30:11 by mpetruno          #+#    #+#             */
+/*   Updated: 2018/08/14 18:38:50 by mpetruno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+
+//BONUSES:
+//use single static variable
+//manage multiple file descriptors
+
+int	get_next_line(const int fd, char **line)
+{
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpetruno <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/14 18:30:32 by mpetruno          #+#    #+#             */
+/*   Updated: 2018/08/14 18:37:32 by mpetruno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+
+# define BUFF_SIZE 10
+
+int	get_next_line(const int fd, char **line);
+
+#endif
